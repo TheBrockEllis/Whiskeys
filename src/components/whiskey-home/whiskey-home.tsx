f1c6d4d4a196e2de4ae86ffe5069c8ffd1f61e62
Brock Ellis <brock@sharproot.com>
2019-07-09 12:52:57 -0500

    Adds filter styles, fetch articles and whiskeys on WillLoad

--- src/components/whiskey-home/whiskey-home.tsx
+++ src/components/whiskey-home/whiskey-home.tsx
@@ -1,5 +1,6 @@
 import { Component, h, State } from '@stencil/core';
 import { Whiskey } from '../../types/whiskey';
+import { Article } from '../../types/article';
 
 @Component({
   tag: 'whiskey-home',
@@ -8,12 +9,15 @@ import { Whiskey } from '../../types/whiskey';
 })
 export class AppHome {
   @State() whiskeys: Whiskey[] = [];
+  @State() articles: Article[] = [];
 
   componentWillLoad() {
-    fetch('assets/fixtures/whiskies.json')
-      .then((response: Response) => response.json())
-      .then(response => {
-        this.whiskeys = response;
+    let whiskiesPromise = fetch('assets/fixtures/whiskies.json').then((response: Response) => response.json());
+    let articlesPromise = fetch('assets/fixtures/articles.json').then((response: Response) => response.json());
+
+    Promise.all([whiskiesPromise, articlesPromise]).then( (values: any) => {
+      this.whiskeys = values[0];
+      this.articles = values[1];
     });
   }
 
@@ -21,11 +25,12 @@ export class AppHome {
     return (
       <div class='whiskey-home'>
         
-        <ul>
-          <li>Place</li>
-          <li>Place</li>
-          <li>Place</li>
-          <li>Place</li>
+        <ul class='whiskey-filter'>
+          <li class='active'>All</li>
+          <li>Highland</li>
+          <li>Lowland</li>
+          <li>Islands</li>
+          <li>Isley</li>
         </ul>
 
         <div class='container'>
